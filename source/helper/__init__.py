3391d8ca927cc217e64237a39121ff517b87ef56
Stacey Beck <staceyrivet@gmail.com>
2022-04-05 14:51:26 -0700

    reorganizing notebooks

---
+++ source/helper/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Project:     teambrainiac
+# @Filename:    __init__.py.py
+# @Author:      staceyrivet
+# @Time:        4/5/22 2:48 PM
+# @IDE:         PyCharm
