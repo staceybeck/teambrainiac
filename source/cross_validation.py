01f2637e5f8eb98fb4d320459575c9e05f11fe52
Stacey Beck <staceyrivet@gmail.com>
2022-04-09 22:21:05 -0700

    Updated cv function with halving grid srch

--- source/cross_validation.py
+++ source/cross_validation.py
@@ -14,7 +14,7 @@
 from sklearn.svm import SVC
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit, HalvingGridSearchCV
 from sklearn.exceptions import ConvergenceWarning
 import warnings
 import tqdm
@@ -24,7 +24,7 @@ import tqdm
 
 
 
-def time_series_cv(X, y, max_train, test_size, splits):
+def time_series_cv(X, y, max_train, test_size, splits, gd_srch, param_dict, file_name):
 
     """
     :param X        : nd.array shape (time points, 237979)
@@ -39,24 +39,39 @@ def time_series_cv(X, y, max_train, test_size, splits):
     accuracy_ = []
     it = 0
 
-    for train_index, test_index in tqdm.tqdm(tscv.split(X)):
-        it += 1
-        # print("TRAIN:", train_index, "TEST:", test_index)
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+    if gd_srch == True:
+        clf = SVC(random_state = 42, max_iter = 1000, class_weight = balanced)
+        param_search = param_dict
+        grid = HalvingGridSearchCV(estimator= clf,
+                                   cv = tscv,
+                                   param_grid = param_search)
+        grid.fit(X, y)
+        print("Uploading gridsearch results to cloud...")
+        s3_upload(grid.cv_results_, file_name, 'csv')
+        print("Best parameters: ", grid.best_params_)
+        print('Best estimator: ', grid.best_estimator_)
+        print("Best score: ", grid.best_score_)
 
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", category=ConvergenceWarning)
-            clf = SVC(C=5.0, class_weight='balanced', max_iter=1000, random_state=42)
-            print(f"Fitting Classifier for iteration number {it}")
-            clf.fit(X_train, y_train)
+    else:
 
-        print("Predicting...")
-        y_pred = clf.predict(X_test)
+        for train_index, test_index in tqdm.tqdm(tscv.split(X)):
+            it += 1
+            # print("TRAIN:", train_index, "TEST:", test_index)
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
 
-        # Model Accuracy
-        score = accuracy_score(y_test, y_pred)
-        print(f"Cross Validation Split {it} Accuracy score:", score)
-        accuracy_.append(score)
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=ConvergenceWarning)
+                clf = SVC(C=5.0, class_weight='balanced', max_iter=1000, random_state=42)
+                print(f"Fitting Classifier for iteration number {it}")
+                clf.fit(X_train, y_train)
 
-    print("Mean Accuracy: {}".format(np.mean(accuracy_)))
+            print("Predicting...")
+            y_pred = clf.predict(X_test)
+
+            # Model Accuracy
+            score = accuracy_score(y_test, y_pred)
+            print(f"Cross Validation Split {it} Accuracy score:", score)
+            accuracy_.append(score)
+
+        print("Mean Accuracy: {}".format(np.mean(accuracy_)))
