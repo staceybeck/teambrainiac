05a0cf9181cfd949b67e4e347443e9fc3b28e8ff
Stacey Beck <staceyrivet@gmail.com>
2022-04-16 14:01:13 -0700

    landing_page.py dropdown and page works

--- source/streamlit/landing_page.py
+++ source/streamlit/landing_page.py
@@ -7,48 +7,73 @@
 # @IDE:         PyCharm
 
 import streamlit as st
+import streamlit.components.v1 as components
 
 
-
-st.title("Team Brainiacs conquer fMRI")
 
 PAGES = [
-    'Brain images',
+    'Brain Images',
     'Chart Metrics',
     'Tables'
 ]
 
-
-
 st.sidebar.title('Explore Data')
+st.session_state.page_select = st.sidebar.radio('Pages', PAGES)
 
-
-page = st.sidebar.radio('Navigation', PAGES)
-
-
-if page == 'Brain images':
+if st.session_state.page_select == 'Brain Images':
+    st.title("Brain States")
     st.sidebar.write("""
-            ## About
+            ## Brain Images
             These images display the BOLD (Blood oxygen level dependent) voxel signal in the brain
             thresholded at a specific value. Areas in white/yellow indicate areas of the brain where
-            blood volume increased and increases in oxygen exchange occur. This indicates that this part 
-            of the brain is active when the image in the MRI is captured. Areas in the brain that are blue are
+            blood volume is increased and where increases in oxygen exchange occur. This indicates that this part 
+            of the brain is active when the image in the MR machine is captured. Areas in the brain that are blue are
             when the blood volume and oxygen exchange decrease, indicating that these areas of the brain as becoming
             less active.
                 
             """)
+    display = ('Young Adult Whole Brain',
+               'Young Adult without mPFC',
+               'Young Adult Prefrontal Cortex',
+               'Adolescent Whole Brain',
+               'Adolescent Nucleus Accumbens',
+               'Adolescent Prefrontal Cortex',
+               )
 
+    value = 0
+    options = list(range(len(display)))
 
-elif page == 'Chart Metrics':
+    def get_html(value):
+        #print(value)
+        if value == 0:
+            HtmlFile = open("YA_YA_mask_1.html", 'r',
+                            encoding='utf-8')
+            source_code = HtmlFile.read()
+            print(source_code)
+            components.html(source_code, height=250)
+
+        if value == 1:
+            HtmlFile = open("YA_YA_masksubmPFC_1.html", 'r',
+                            encoding='utf-8')
+            source_code = HtmlFile.read()
+            print(source_code)
+            components.html(source_code, height=250)
+
+    value = st.selectbox("Choose the type of brain activations to view:", options, format_func=lambda x: display[x])
+    get_html(value)
+
+if st.session_state.page_select == 'Chart Metrics':
+    st.title("Chart Metrics")
     st.sidebar.write("""
-            ## About
+            ## Chart Metrics
             The charts on this page are metrics captured when running our models on single subjects or when grouping subjects
             by age. Adolescent are ages 16-19 years old and Young Adults are older than 19 years old. 
             """)
 
-else:
+if st.session_state.page_select == 'Tables':
+    st.title("Tables")
     st.sidebar.write("""
-            ## About
+            ## Tables
             The tables on this page provide use with useful information about our decision choices for further
             preprocessing of our data. 
             """)
