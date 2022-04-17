f63a0c1628760e09118d857763014b7e43aad705
Stacey Beck <staceyrivet@gmail.com>
2022-04-16 22:43:33 -0700

    Updating train svc params for adol

--- source/group_svm/train.py
+++ source/group_svm/train.py
@@ -59,14 +59,15 @@ def run_grp_svm_model(data, mask_type, group_sub_ids, runs_train, runs_val, runs
 
         runs_id = [i + 1 for i in runs_train]
 
-        clf = SVC(C=10.0,
+        clf = SVC(C = 0.03, #C=10.0,
                   class_weight='balanced',
                   max_iter=1000,
                   random_state=42,
                   probability=True,
-                  gamma = 'auto'
+                  #gamma = 'auto' # for young adult
+                  gamma='scale' #For adolescent
                   )
-        print(f"Fitting the model for {mask_type}...")
+        print(f"Fitting the model for {mask_type} on Train and then will Test...")
         clf.fit(X, y)
 
         # Calculate metrics
