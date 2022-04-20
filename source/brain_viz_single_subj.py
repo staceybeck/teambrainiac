0f89463db07eb4a4e3347baef937cb34dfb31f53
mesoules <100222694+mesoules@users.noreply.github.com>
2022-04-20 09:25:31 -0400

    figuring out why it won't save

--- source/brain_viz_single_subj.py
+++ source/brain_viz_single_subj.py
@@ -126,10 +126,12 @@ def plot_decision_scores(scores,labels,title_str,subject_type,run,outfname=None)
   ax.plot(labels, lw=3, c='#446CCF',label='True Labels')
   ax.axhline(y=0,c='r',linestyle='--',label='Decision Function Cutoff')
   ax.plot(scores,c='k',lw=0.2,linestyle='-')
-  print(f'Saving to {outfname}')
+  ax.set_title(f'{subject_type} Decision Function Scores for {title_str} on {run}')
   if outfname!=None:
+    print(f'Saving to {outfname}')
     fig.savefig(f'outfname{subject_type}_descf_{title_str}.png',dpi=200)
-  plt.show()
+  else:
+    plt.show()
     
 #   x = [0]
 #   y = [0]
