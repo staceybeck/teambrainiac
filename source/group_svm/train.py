a3d0c6211350765a80c1995428d03df91fcb9b54
Stacey Beck <staceyrivet@gmail.com>
2022-04-22 17:49:39 -0700

    fix train

--- source/group_svm/train.py
+++ source/group_svm/train.py
@@ -44,21 +44,26 @@ def run_grp_svm_model(data, mask_type, group_sub_ids, runs_train, runs_val, runs
 
         runs_id = [i + 1 for i in runs_train]
 
-        clf = SVC(C=10.0,
-                  class_weight='balanced',
-                  max_iter=1000,
-                  random_state=42,
-                  probability = True,
-                  gamma = 'auto'
-                  )
+        if data_type == "AD_detrend":
+            clf = SVC(C=5.0 #10
+                      class_weight='balanced',
+                      max_iter=1000,
+                      random_state=42,
+                      probability=True,
+                      gamma='scale' #For adolescent
+                      )
+        else:
+            clf = SVC(C=10.0,
+                      class_weight='balanced',
+                      max_iter=1000,
+                      random_state=42,
+                      probability = True,
+                      gamma = 'auto'
+                      )
         print(f"Fitting the model for {mask_type}...")
         clf.fit(X, y)
 
         model_dict['model'] = clf
-        full_path_name = f'/data/{data_type}_{mask_type}_{runs_id}_model.pkl'
-        filehandler = open(full_path_name, "wb")
-        pickle.dump(subj_mask_model, filehandler)
-        filehandler.close()
 
         # Calculate metrics
         metrics_data = metrics(clf, X, y, X_v, y_v, X_t, y_t, data_type, runs_id, mask_type, m_path_ind)
@@ -67,26 +72,30 @@ def run_grp_svm_model(data, mask_type, group_sub_ids, runs_train, runs_val, runs
 
         runs_id = [i + 1 for i in runs_train]
 
-        clf = SVC(C=10.0,
-                  class_weight='balanced',
-                  max_iter=1000,
-                  random_state=42,
-                  probability=True,
-                  #gamma = 'auto' # for young adult
-                  gamma='scale' #For adolescent
-                  )
+        if data_type == "AD_detrend":
+            clf = SVC(C=5.0 #10
+                      class_weight='balanced',
+                      max_iter=1000,
+                      random_state=42,
+                      probability=True,
+                      gamma='scale' #For adolescent
+                      )
+        else:
+            clf = SVC(C=10.0,
+                      class_weight='balanced',
+                      max_iter=1000,
+                      random_state=42,
+                      probability=True,
+                      gamma = 'auto' # for young adult
+                      )
         print(f"Fitting the model for {mask_type} on Train and then will Test...")
         clf.fit(X, y)
 
         model_dict['model'] = clf
-        full_path_name = f'/data/{data_type}_{mask_type}_{runs_id}_model.pkl'
-        filehandler = open(full_path_name, "wb")
-        pickle.dump(subj_mask_model, filehandler)
-        filehandler.close()
 
         # Calculate metrics
         metrics_data = metrics(clf, X, y, False, False, X_t, y_t, data_type, runs_id, mask_type, m_path_ind)
 
 
     # Return metrics data
-    return metrics_data
+    return model_dict, metrics_data
