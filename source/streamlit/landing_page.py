6a421a8322d5782c45811bec13a4541c4d1f7e0c
Stacey Beck <staceyrivet@gmail.com>
2022-04-25 13:01:52 -0700

    rearrange text

--- source/streamlit/landing_page.py
+++ source/streamlit/landing_page.py
@@ -54,7 +54,19 @@ if st.session_state.page_select == 'Brain Images':
     def get_html(value):
         #print(value)
         if value == 0:
-            st.write("Signals thresholded at 95%")
+            st.write("Signals thresholded at 95%"
+                     "\n")
+            st.write(
+                "This is an interactive brain map. We trained a Support Vector Machine on Young Adult Brains (aged 25 - 27) "
+                "and Adolescent Brains (aged 14 - 16) to predict states of up-regulation and down-regulation - meaning "
+                "an uptake in blood-oxygen volume or decrease in volume during an impulse-task. Voxel intensities that are yellow/red"
+                " are active brain areas during a task and voxel intensities that are dark-blue/light-blue are areas where blood and "
+                "oxygen levels are decreasing."
+                "\n"
+                "In our research, we have found that the Young Adult models predicted better than the Adolescent models. "
+                " You can see clear and distinct voxel clusters in the output from the "
+                " Young Adult model (dark red and blue shades) compared to the Adolescent model output where "
+                "voxel intensities are scattered and dampened (light blue and yellow).")
             HtmlFile = open("/app/teambrainiac/source/streamlit/YA_detrend_mask_1.html", 'r', encoding='utf-8')
             source_code = HtmlFile.read()
             print(source_code)
@@ -71,20 +83,20 @@ if st.session_state.page_select == 'Brain Images':
                 height=250
             )
 
-            st.write("This is an interactive brain map. We trained a Support Vector Machine on Young Adult Brains (aged 25 - 27) "
-                     "and Adolescent Brains (aged 14 - 16) to predict states of up-regulation and down-regulation - meaning "
-                     "an uptake in blood-oxygen volume or decrease in volume during an impulse-task. Voxel intensities that are yellow/red"
-                     " are active brain areas during a task and voxel intensities that are dark-blue/light-blue are areas where blood and "
-                     "oxygen levels are decreasing."
-                     "\n"
-                     "In our research, we have found that the Young Adult models predicted better than the Adolescent models. "
-                     " You can see clear and distinct voxel clusters in the output from the "
-                     " Young Adult model (dark red and blue shades) compared to the Adolescent model output where "
-                     "voxel intensities are scattered and dampened (light blue and yellow).")
-
         if value == 1:
 
-            st.write("Signals thresholded at 95%")
+            st.write("Signals thresholded at 95%"
+                     "\n")
+            st.write(
+                "These are interactive brain maps showing the Medial Prefrontal Cortex (mPFC) - an area of the brain researchers "
+                "believe to be involved in the impulse-reward system. Each interactive visual is set to the same cut coordinates. We removed"
+                " the grid cross since these areas of the brain are small to visualize. "
+                "\n"
+                "In our Young Adult model, we can see there are large areas of the mPFC that are down-regulating, "
+                "meaning a decrease in activation during the impulse-reward task that the subjects are performing while "
+                " in the magnetic resonance (MR) machine. "
+                "In the Adolescent model, we see more yellow intensities, which imply increased voxel activity (up-regulation) "
+                " in this region.")
             HtmlFile = open("/app/teambrainiac/source/streamlit/YA_detrend_mPFC_nocross.html", 'r',
                             encoding='utf-8')
             source_code = HtmlFile.read()
@@ -104,19 +116,19 @@ if st.session_state.page_select == 'Brain Images':
             )
 
 
-
-            st.write("These are interactive brain maps showing the Medial Prefrontal Cortex (mPFC) - an area of the brain researchers "
-                     "believe to be involved in the impulse-reward system. Each interactive visual is set to the same cut coordinates. We removed"
-                     " the grid cross since these areas of the brain are small to visualize. "
-                     "\n"
-                     "In our Young Adult model, we can see there are large areas of the mPFC that are down-regulating, "
-                     "meaning a decrease in activation during the impulse-reward task that the subjects are performing while "
-                     " in the magnetic resonance (MR) machine. "
-                     "In the Adolescent model, we see more yellow intensities, which imply increased voxel activity (up-regulation) "
-                     " in this region.")
-
         if value == 2:
-            st.write("Signals thresholded at 95%")
+            st.write("Signals thresholded at 95%"
+                     "\n")
+
+            st.write(
+                "These are interactive brain maps showing the Nucleus Accumbens (NAcc)- an area of the brain researchers "
+                "know to be involved in the impulse-reward system and brain disorders. Each interactive visual is set to the same cut coordinates. We removed"
+                " the grid cross since these areas of the brain are small to visualize."
+                ""
+                "\n"
+                " In our Young Adult model, we can see there are large clusters of red voxels in the NAcc, which imply these "
+                " subjects are increasing activation during the impulse-reward task that they are performing in while in the MR machine."
+            )
 
             HtmlFile = open("/app/teambrainiac/source/streamlit/YA_detrend_nacc_aal_nocross.html", 'r',
                             encoding='utf-8')
@@ -135,15 +147,6 @@ if st.session_state.page_select == 'Brain Images':
                 source_code,
                 height=250
             )
-            st.write(
-                "These are interactive brain maps showing the Nucleus Accumbens (NAcc)- an area of the brain researchers "
-                "know to be involved in the impulse-reward system and brain disorders. Each interactive visual is set to the same cut coordinates. We removed"
-                     " the grid cross since these areas of the brain are small to visualize."
-                ""
-                "\n"
-                " In our Young Adult model, we can see there are large clusters of red voxels in the NAcc, which imply these "
-                " subjects are increasing activation during the impulse-reward task that they are performing in while in the MR machine."
-                )
 
     value = st.selectbox("Interactive Brain Visualization. Choose the type of brain activation to view and use your cursor to move the grid cross:",
                          options,
@@ -173,6 +176,15 @@ if st.session_state.page_select == "Chart Metrics":
     """)
 
     st.subheader ("Young Adult Whole Brain Mask Model Decision Scores")
+    st.write(" When we trained our young adult models on the support vector machine (SVM) the model output a  "
+             "score for each time point (essentially a score for one full 3D brain image). That score represents a  "
+             "distance between the important voxels to the classifier's decision boundary. As in, how far away "
+             " are we from making a good prediction? "
+             ""
+             "\n"
+             "You can see how the young adult decision scores follow the true labels of the data and do so better "
+             " than the adolescent model scores displayed further down this page. This means that the classifier is "
+             " able to predict the brain states in Young Adults.")
     st.write(""
               "\n"
               "\n"
@@ -195,17 +207,13 @@ if st.session_state.page_select == "Chart Metrics":
              clamp=False,
              channels="RGB",
              output_format="auto")
-    st.write(" When we trained our young adult models on the support vector machine (SVM) the model output a  "
-             "score for each time point (essentially a score for one full 3D brain image). That score represents a  "
-             "distance between the important voxels to the classifier's decision boundary. As in, how far away "
-             " are we from making a good prediction? "
-             ""
-             "\n"
-             "You can see how the young adult decision scores follow the true labels of the data and do so better "
-             " than the adolescent model scores displayed below. This means that the classifier is able to predict t"
-             " he brain states in Young Adults.")
 
     st.subheader("Adolescent Whole Brain Mask Model Decision Scores")
+    st.write("You can see how the adolescent decision scores have a hard time following the true labels of the data"
+             " in Run 2. However, in Run 3, the decision scores appear to be following the curves of the true labels "
+             " better. This suggests that adolescents are learning to regulate their brain activity from one run to "
+             " the next while in the scanner, and that the classifier is learning to predict better as a result.")
+
     st.write("\n"
              "\n"
              "Run 2:"
@@ -227,12 +235,6 @@ if st.session_state.page_select == "Chart Metrics":
              clamp=False,
              channels="RGB",
              output_format="auto")
-    st.write("You can see how the adolescent decision scores have a hard time following the true labels of the data"
-             " in Run 2. However, in Run 3, the decision scores appear to be following the curves of the true labels "
-             " better. This suggests that adolescents are learning to regulate their brain activity from one run to "
-             " the next while in the scanner, and that the classifier is learning to predict better as a result.")
-
-
 
 
 
@@ -321,13 +323,6 @@ if st.session_state.page_select == 'Exploration':
              channels="RGB",
              output_format="auto")
 
-st.sidebar.write("\n"
-                 "\n"
-                 "Authors: Stacey Rivet Beck"
-                 "\n"
-                 "\n"
-                 ""
-                 )
 gitlink = '[Project on GitHub](https://github.com/yecatstevir/teambrainiac)'
 st.sidebar.markdown(gitlink, unsafe_allow_html=True)
 
